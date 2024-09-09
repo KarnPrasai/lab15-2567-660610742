2c29fe235c193dbc5848fd53687fb9ef710249ea
KarnPrasai <karn_prasai@cmu.ac.th>
2024-09-09 22:28:35 +0700

    finished lab

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,7 +48,9 @@ const schema = z
     }),
     hasCoupon: z.boolean(),
     coupon: z.string(),
-    password: z.string(),
+    password: z.string()
+    .min(6, { message: "Password must contain at least 6 characters" })
+    .max(12, { message: "Password must not excced 12 characters"}),
     confirmPassword: z.string(),
   })
   .refine(
@@ -69,7 +71,19 @@ const schema = z
       message: "Invalid coupon code",
       path: ["coupon"],
     }
-  );
+  )
+  .refine(
+    (data) => {
+      if(data.password === data.confirmPassword)
+          return true;
+      return false;
+    },
+    {
+      message: "Password does not match",
+      path: ["confirmPassword"],
+    }
+  )
+  ;
 
 export default function Home() {
   const [opened, { open, close }] = useDisclosure(false);
@@ -96,11 +110,18 @@ export default function Home() {
     //TIP : get value of currently filled form with variable "form.values"
 
     if (form.values.plan === "funrun") price = 500;
+    if (form.values.plan === "mini") price = 800;
+    if (form.values.plan === "half") price = 1200;
+    if (form.values.plan === "full") price = 1500;
     //check the rest plans by yourself
     //TIP : check /src/app/libs/runningPlans.js
-
+    
     //check discount here
-
+    if(form.values.hasCoupon === true && form.values.coupon === "CMU2023")
+    {
+      price = price * 70 / 100;
+    }
+    
     return price;
   };
 
@@ -188,6 +209,7 @@ export default function Home() {
         </form>
 
         {/* <Footer year={2023} fullName="Chayanin Suatap" studentId="650610560" /> */}
+        <Footer  year={2024} fullName="Karn Prasai" studentId="660610742"/>
       </Container>
 
       <TermsAndCondsModal opened={opened} close={close} />
